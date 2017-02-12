13cbdeb3fea16c60e58c6eb9aaef3e793af80588
vishal1796 <vkyadav.4242@gmail.com>
2017-02-13 01:28:53 +0530

    phew! training and loss done

--- data_util.py
+++ data_util.py
@@ -1,4 +1,28 @@
+import os
 import torch
+from torchvision.transforms import Compose, CenterCrop, ToTensor, Scale
+from dataset import DatasetFromFolder
+
+transform = transforms.Compose([transforms.Scale(args.image_size),
+                               transforms.CenterCrop(args.image_size),
+                               transforms.ToTensor(),
+                               transforms.Lambda(lambda x: x.mul(255))])
+def get_training_set():
+    root_dir = download_bsd300()
+    train_dir = os.join(root_dir, "train")
+    return DatasetFromFolder(train_dir, input_transform=transform)
+
+
+def download_convert_vgg16_model():
+    if not os.path.exists('vgg16feature.pth'):
+        if not os.path.exists('vgg16.t7'):
+            os.system('wget http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/vgg16.t7')
+        vgglua = load_lua('vgg16.t7')
+        vgg = models.VGGFeature()
+        for (src, dst) in zip(vgglua.parameters()[0], vgg.parameters()):
+            dst[:] = src[:]
+        torch.save(vgg.state_dict(), 'vgg16feature.pth')
+
 
 def vgg_preprocessing(batch):
     tensortype = type(batch.data)
@@ -16,3 +40,13 @@ def batch_rgb_to_bgr(batch):
     batch = torch.cat((b, g, r))
     batch = batch.transpose(0, 1)
     return batch
+
+
+# result: RGB CxHxW [0,255] torch.FloatTensor
+def load_image(filename, size=None):
+    img = Image.open(filename)
+    if size is not None:
+        img = img.resize((size, size), Image.ANTIALIAS)
+    img = np.array(img).transpose(2, 0, 1)
+    img = torch.from_numpy(img).float()
+    return img
--- train.py
+++ train.py
@@ -6,18 +6,18 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from model import FastStyleNet
 from data import get_training_set
-from data_util import batch_rgb_to_bgr
+from data_util import batch_rgb_to_bgr, load_image
 from loss import loss_function
 
-parser = argparse.ArgumentParser(description='Fast Neural style transfer with PyTorch.')
+parser = argparse.ArgumentParser(description='Fast Neural style transfer using PyTorch.')
 parser.add_argument('style_image_path', metavar='ref', type=str, help='Path to the style reference image.')
 parser.add_argument("data_path", type=str, help="Path to training images")
-parser.add_argument("--content_weight", type=float, default=10., help='Content weight')
-parser.add_argument("--style_weight", type=float, default=1., help='Style weight')
-parser.add_argument("--tv_weight", type=float, default=8.5e-5, help='Total Variation Weight')
+parser.add_argument("--content_weight", type=float, default=1.0, help='Content weight')
+parser.add_argument("--style_weight", type=float, default=5.0, help='Style weight')
 parser.add_argument("--image_size", dest="img_size", default=256, type=int, help='Output Image size')
 parser.add_argument("--epochs", default=2, type=int, help='Number of epochs')
 parser.add_argument("--batchSize", default=4, type=int, help='Number of images per epoch')
+parser.add_argument('--lr', type=float, default=0.001, help='Learning Rate. Default=0.01')
 parser.add_argument('--cuda', action='store_true', help='use cuda?')
 args = parser.parse_args()
 
@@ -37,17 +37,16 @@ if args.cuda:
     model.cuda()
 optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
-style = tensor_load_rgbimage(args.style_image, args.style_size)
-style = style.repeat(args.batch_size, 1, 1, 1)
-style = batch_rgb_to_bgr(style)
+style_image = load_image(args.style_image, args.style_size)
+style_image_batch = style.repeat(args.batch_size, 1, 1, 1)
+style_image_batch = batch_rgb_to_bgr(style_image_batch)
 if args.cuda:
-    style = style.cuda()
-xs = Variable(style, volatile=True)
-
+    style_image_batch = style_image_batch.cuda()
+xs = Variable(style_image_batch, volatile=True)
 
 print('===> Training model')
 for epoch in range(args.epochs):
-    for batch in data_loader:
+    for iteration, batch in enumerate(data_loader, 1):
         data = batch[0].clone()
         data = batch_rgb_to_bgr(data)
         if args.cuda:
@@ -58,6 +57,6 @@ for epoch in range(args.epochs):
         loss = loss_function(args.content_weight, args.style_weight, yc, ys, y_hat)
         loss.backward()
         optimizer.step()
-        print('===> Epoch[{}] batch({}/{}): Loss: {:.4f}'.format(epoch, i, n_iter, loss.data[0]))
+        print("===> Epoch[{}]({}/{}): Loss: {:.4f}".format(epoch, iteration, len(data_loader), loss.data[0]))
     torch.save(model.state_dict(), 'model_{}.pth'.format(epoch))
 torch.save(model.state_dict(), 'model.pth')
