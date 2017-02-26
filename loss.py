e9daaf3a939774ed8aa5c6cb7d64a81fbcee717a
Vishal Kumar <vkyadav.4242@gmail.com>
2017-02-26 18:20:53 +0000

    check for args cuda

--- loss.py
+++ loss.py
@@ -1,6 +1,5 @@
 import os
 import torch
-from torch.nn import MSELoss
 from torch.utils.serialization import load_lua
 import models
 from data_utils import vgg_preprocessing
@@ -27,19 +26,22 @@ def loss_function(content_weight, style_weight, yc, ys, y_hat):
     vgg16_model()
     vgg = models.VGGFeature()
     vgg.load_state_dict(torch.load('vgg16feature.pth'))
-    if args.cuda:
-        vgg.cuda()
+    vgg.cuda()
         
     vgg_preprocessing(yc)
     vgg_preprocessing(ys)
+    vgg_preprocessing(y_hat)
+    
+    loss_func = torch.nn.MSELoss()
+    
     feature_c = vgg(yc)
     feature_hat = vgg(y_hat)
-    feat_loss = content_weight * MSELoss(feature_hat[2], Variable(feature_c[2].data, requires_grad=False))
+    feat_loss = content_weight * loss_func(feature_hat[2], Variable(feature_c[2].data, requires_grad=False))
 
     feature_s = vgg(Variable(ys, volatile=True))
     gram_s = [gram_matrix(y) for y in feature_s]
     gram_hat = [gram_matrix(y) for y in feature_hat]
     for m in range(0, len(feature_hat)):
-        style_loss += style_weight * MSELoss(gram_hat[m], Variable(gram_s[m].data, requires_grad=False))
+        style_loss += style_weight * loss_func(gram_hat[m], Variable(gram_s[m].data, requires_grad=False))
 
     return style_loss + feat_loss
